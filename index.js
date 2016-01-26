5acadae30413d25f9c039dcf8977daa4d66b40d5
Ewan Dennis <ewandennis@users.noreply.github.com>
2016-01-26 12:00:19 +0000

    Pared down: just a search interface

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express')
   , bodyParser = require('body-parser')
   , morgan = require('morgan')
   , app = express()
-  , raffleRouter = require('./routes/raffles')
   , srv = require('http').Server(app);
 
 app.use(bodyParser.json({
@@ -13,7 +12,26 @@ app.use(morgan('dev'));
 
 app.use('/', express.static(__dirname + '/ui/static/'));
 
-app.use('/api/raffles', raffleRouter);
+// TODO: query WEBHOOK_CONSUMER_DB
+app.get('/api/raffles', function(req, res) {
+  res.json([
+    {
+      smtp_from: "bob@bob.com",
+      smtp_to: "elixir@hey.avocado.industries",
+      subject: "Gimme da goods!"
+    },
+    {
+      smtp_from: "jim@jim.com",
+      smtp_to: "elixir@hey.avocado.industries",
+      subject: "I love swag"
+    },
+    {
+      smtp_from: "sue@sue.com",
+      smtp_to: "elixir@hey.avocado.industries",
+      subject: "Cialis!"
+    }
+  ]);
+});
 
 srv.listen(process.env.PORT || 3000, function() {
   console.log('Listening on port ' + srv.address().port);
