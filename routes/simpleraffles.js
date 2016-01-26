3a87ac631dae04f723c814f0a1e7318c143bcfac
Ewan Dennis <ewandennis@users.noreply.github.com>
2016-01-26 19:53:57 +0000

    Added winner endpoint impl

--- routes/simpleraffles.js
+++ routes/simpleraffles.js
@@ -6,21 +6,40 @@ var q = require('q')
 
 module.exports = router;
 
+function db() {
+  return pgp(process.env.HEROKU_POSTGRESQL_CHARCOAL_URL);
+}
+
 router.get('/:raffleId', function(req, res) {
-  pgp(process.env.HEROKU_POSTGRESQL_CHARCOAL_URL).one(
+  db().one(
     "SELECT COUNT(*) AS cnt, MIN(created) AS startdate, MAX(created) as enddate FROM request_dump.relay_messages WHERE smtp_to = $1 || '@' || $2",
     [req.params.raffleId, process.env.RCPT_DOMAIN]
   ).then(function(result) {
-    return res.json({results: {
-      num_entries: result.cnt,
-      first_received: result.startdate,
-      last_received: result.enddate
-    }
-  });
+    return res.json({
+      results: {
+        num_entries: result.cnt,
+        first_received: result.startdate,
+        last_received: result.enddate
+      }
+    });
   }).fail(function(err) {
     res.status(500).send({errors: [err]});
   });
 });
 
 router.get('/raffleId/winner', function(req, res) {
+  db().one("SELECT message_id, smtp_from, smtp_to, subject, created FROM relay_messages " +
+    "WHERE smtp_to = $1 || '@' || $2 offset floor(random()*(select count(*) " +
+    "from request_dump.relay_messages WHERE smtp_to = $1 || '@' || $2 limit 1",
+    [req.params.raffleId, process.env.RCPT_DOMAIN]
+  ).then(function(winner) {
+    return res.json({
+      results: {
+        winner_address: winner.smtp_from,
+        winner_sent_at: winner.created
+      }
+    });
+  }).fail(function (err) {
+    res.status(500).send({errors: [err]});
+  });
 });
