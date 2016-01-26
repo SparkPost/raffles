a2b1b8bb4fbd92436f4a46190ca295d195e96a30
Ewan Dennis <ewandennis@users.noreply.github.com>
2016-01-26 19:05:43 +0000

    Added pre-raffle time window reporting

--- routes/simpleraffles.js
+++ routes/simpleraffles.js
@@ -8,10 +8,15 @@ module.exports = router;
 
 router.get('/:raffleId', function(req, res) {
   pgp(process.env.HEROKU_POSTGRESQL_CHARCOAL_URL).one(
-    "SELECT COUNT(*) FROM request_dump.relay_messages WHERE smtp_to = $1 || '@' || $2",
+    "SELECT COUNT(*) AS cnt, MIN(created) AS startdate, MAX(created) as enddate FROM request_dump.relay_messages WHERE smtp_to = $1 || '@' || $2",
     [req.params.raffleId, process.env.RCPT_DOMAIN]
-  ).then(function(cnt) {
-    return res.json({results: {num_entries: cnt}});
+  ).then(function(result) {
+    return res.json({results: {
+      num_entries: result.cnt,
+      first_received: result.startdate,
+      last_received: result.enddate
+    }
+  });
   }).fail(function(err) {
     res.status(500).send({errors: [err]});
   });
