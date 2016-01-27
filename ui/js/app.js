5c7cf8a7c5dc2fb4ce3044f48868623b77ec992c
Ewan Dennis <ewandennis@users.noreply.github.com>
2016-01-27 14:41:30 +0000

    UI: winner display, alerts

--- ui/js/app.js
+++ ui/js/app.js
@@ -1,30 +1,64 @@
 'use strict';
 
-var rafflesApp = angular.module('rafflesApp', ['rafflesControllers']);
+var rafflesApp = angular.module('rafflesApp', ['rafflesControllers', 'ui.bootstrap']);
 
 var rafflesControllers = angular.module('rafflesControllers', []);
 
 rafflesControllers.controller('RaffleListCtrl', ['$scope', '$http', '$log',
   function($scope, $http, $log) {
-    $scope.raffles = [];
 
-    $http({
-      method: 'GET',
-      url: '/raffles',
-    }).then(function(response) {
-      $scope.raffles = response.data.results;
-    }).catch(function(err) {
-      console.log('/raffles error: ' + JSON.stringify(err));
-    });
+    $scope.raffles = [];
+    $scope.to_date = new Date();
+    $scope.to_date.setHours(12);
+    $scope.to_date.setMinutes(0);
+    $scope.to_date.setSeconds(0);
+    $scope.to_date.setMilliseconds(0);
+    $scope.from_date = new Date($scope.to_date - 1000*60*60*24);
+
+    $scope.alerts = [];
+    $scope.closeAlert = function(idx) {
+      $scope.alerts.splice(idx, 1);
+    };
+
+    function showInfo(msg) {
+      $scope.alerts.push({type: 'success', msg: msg});
+    }
+    function showWarning(msg) {
+      $scope.alerts.push({type: 'warning', msg: msg});
+    }
+    function showError(msg) {
+      $scope.alerts.push({type: 'danger', msg: msg});
+    }
+    
+    $scope.updateView = function() {
+      $http({
+        method: 'GET',
+        url: '/raffles',
+        params: {
+          from: $scope.from_date.toISOString(),
+          to: $scope.to_date.toISOString()
+        }
+      }).then(function(response) {
+        $scope.raffles = response.data.results;
+      }).catch(function(err) {
+        showError('While GETting from /raffles: ' + err.statusText);
+      });
+    };
 
     $scope.pickWinner = function(localpart) {
+      $log.log("from: " + $scope.from_date.toISOString());
+      $log.log("to: " + $scope.to_date.toISOString());
       $http({
         method: 'GET',
         url: '/raffles/' + localpart + '/winner'
       }).then(function(response) {
-        if (response.errors) {
+        if (response.data.errors) {
+          showWarning(response.errors.join('<br/>'));
         } else {
+          showInfo('Winner of raffle "' + localpart + '": ' + response.data.results.winner_address);
         }
       });
     };
+
+    $scope.updateView();
   }]);
