fba975eb57f9d856d1be3306aeec5f0e526d2e4f
Aydrian <aydrian@gmail.com>
2016-04-08 13:10:43 -0400

    Added confirmation email using default and raffle specific templates (#6)
    * Added confirmation email using default and raffle specific templates
    * Ewan's feedback

--- models/relayMessage.js
+++ models/relayMessage.js
@@ -13,5 +13,22 @@ module.exports = {
       json.content.email_rfc822,
       json.content.email_rfc822_is_base64
     ]);
+  },
+  createMany: function(batch) {
+    return db().tx(function(t) {
+      var txdb = this;
+      return batch.map(function(msg) {
+        msg = msg.msys.relay_message;
+        return txdb.none("INSERT INTO request_dump.relay_messages (webhook_id, smtp_from, smtp_to, subject, rfc822, is_base64) " +
+          "VALUES ($1, $2, $3, $4, $5, $6)", [
+          msg.webhook_id,
+          msg.msg_from,
+          msg.rcpt_to,
+          msg.content.subject,
+          msg.content.email_rfc822,
+          msg.content.email_rfc822_is_base64
+        ]);
+      });
+    });
   }
 };
