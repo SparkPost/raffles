89ca41d5e7f74b4ed33c6018c4351b1062b3354a
Cole Furfaro-Strode <colestrode@gmail.com>
2016-06-07 07:04:14 -0400

    FAD-3104 reset auth to original password

--- auth.js
+++ auth.js
@@ -6,7 +6,7 @@ var md5 = require('md5')
   , strat;
 
 strat = new BasicStrategy(function(uid, passwd, next) {
-  if (uid === 'devrel' && md5(passwd) !== '6118fda28fbc20966ba8daafdf836683') {
+  if (uid === 'devrel' && md5(passwd) === '0e7d04e99a4400d4c6964735d411c30b') {
     return next(null, {});
   }
   return next(null, false);
---
+++ ui/js/views/raffles/raffles-controller.js
@@ -0,0 +1,66 @@
+angular.module('rafflesControllers', ['ui.router'])
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('raffles', {
+        url: "/raffles",
+        controller: 'RaffleListCtrl',
+        templateUrl: "js/controllers/raffles.html"
+      });
+  })
+  .controller('RaffleListCtrl', ['$scope', '$http', '$log', function($scope, $http, $log) {
+
+    $scope.raffles = [];
+    $scope.to_date = new Date();
+    $scope.to_date.setHours(12);
+    $scope.to_date.setMinutes(0);
+    $scope.to_date.setSeconds(0);
+    $scope.to_date.setMilliseconds(0);
+    $scope.from_date = new Date($scope.to_date - 1000*60*60*24);
+
+    $scope.alerts = [];
+    $scope.closeAlert = function(idx) {
+      $scope.alerts.splice(idx, 1);
+    };
+
+    function showInfo(msg) {
+      $scope.alerts.push({type: 'success', msg: msg});
+    }
+    function showWarning(msg) {
+      $scope.alerts.push({type: 'warning', msg: msg});
+    }
+    function showError(msg) {
+      $scope.alerts.push({type: 'danger', msg: msg});
+    }
+
+    $scope.getResults = function() {
+      $http({
+        method: 'GET',
+        url: '/raffles',
+        params: {
+          from: $scope.from_date.toISOString(),
+          to: $scope.to_date.toISOString()
+        }
+      }).then(function(response) {
+        $scope.raffles = response.data.results;
+      }).catch(function(err) {
+        showError('While GETting from /raffles: ' + err.statusText);
+      });
+    };
+
+    $scope.pickWinner = function(localpart) {
+      $http({
+        method: 'GET',
+        url: '/raffles/' + localpart + '/winner'
+      }).then(function(response) {
+        if (response.data.errors) {
+          showWarning(response.errors.join('<br/>'));
+        } else {
+          showInfo('Winner of raffle "' + localpart + '": ' + response.data.results.winner_address);
+        }
+      }).catch(function(err) {
+        showError('While GETting from /raffles: ' + err.statusText);
+      });
+    };
+
+    $scope.getResults();
+  }]);
