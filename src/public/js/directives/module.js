b12c26eb051dd607f410ffe32484618cba842af5
Aydrian J. Howard <aydrian@gmail.com>
2017-08-17 12:28:39 -0400

    clean repo

--- src/public/js/directives/module.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// a roll up module to simplify including directives in controllers
-angular.module('rafflesApp.directives', [
-  'rafflesApp.directives.date-picker',
-  'rafflesApp.directives.alert'
-]);
--- src/public/js/views/module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// a roll up module to simplify including controllers in the app
-angular.module('rafflesApp.controllers', [
-  'rafflesApp.controllers.raffleList',
-  'rafflesApp.controllers.entriesList',
-  'rafflesApp.controllers.dashboard'
-]);
