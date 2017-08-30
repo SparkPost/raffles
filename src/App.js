ee4e783dbbe6e839df85a2e00df2d0c1f3de2b1d
Aydrian J. Howard <aydrian@gmail.com>
2017-08-30 16:28:34 -0400

    Style updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import './App.css';
+import React, { Component } from 'react'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container container--content">
+          Hello
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export default App
