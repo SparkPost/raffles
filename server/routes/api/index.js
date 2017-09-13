6e051db9fac419c87ea57d9debdd1680b2780195
Aydrian J. Howard <aydrian@gmail.com>
2017-09-13 16:55:55 -0400

    Authenticate all API routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
+const passport = require('passport')
 
+// Authenticate all API routes
+router.use(passport.authenticate('bearer', { session: false }))
 
 module.exports = router
